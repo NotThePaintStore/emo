d2a3453e555476539e2c79ed899421faeb246dab
NotThePaintStore <ben@bmoore.xyz>
2022-09-19 18:04:24 -0400

    WIP: Add templating and logic

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ import (
 	"net/http"
 )
 
+func serveJSON(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./test.json")
+}
+
 func main() {
 	http.Handle("/", http.RedirectHandler("https://emo.bmoore.xyz/copypaste/", http.StatusSeeOther))
 	fs := http.FileServer(http.Dir("./copypaste/"))
 	http.Handle("/copypaste/", http.StripPrefix("/copypaste", fs))
+	http.HandleFunc("/comments.json", serveJSON)
 	fmt.Println("Server active on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
