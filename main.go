1437d5b0e373461802595d7a6590ddcfb81f0752
NotThePaintStore <ben@bmoore.xyz>
2022-09-20 18:37:24 -0400

    Fix paths

--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 func serveJSON(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./test.json")
+	http.ServeFile(w, r, "./copypaste/test.json")
 }
 
 func main() {
 	http.Handle("/", http.RedirectHandler("https://emo.bmoore.xyz/copypaste/", http.StatusSeeOther))
 	fs := http.FileServer(http.Dir("./copypaste/"))
 	http.Handle("/copypaste/", http.StripPrefix("/copypaste", fs))
-	http.HandleFunc("/comments.json", serveJSON)
+	http.HandleFunc("/copypaste/comments.json", serveJSON)
 	fmt.Println("Server active on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
